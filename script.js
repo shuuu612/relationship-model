7b035f79d313f3a1d0cfaa19ed1761e7f752a618
shunsuke <shun.8000.sf@gmail.com>
2022-06-30 19:12:06 +0900

    add function to draw paths

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const context = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const button = document.getElementById('button');
+const playIcon = document.getElementById('play');
+const stopIcon = document.getElementById('stop');
+
+button.addEventListener('click', handleClick);
+playIcon.style.cssText = 'display:none';
+
+let requestID;
+let isPlaying = true;
 
 class Circle {
   constructor(name, x, y, color, size, activity, coordinationLevel) {
@@ -17,9 +23,11 @@ class Circle {
     this.size = size;
     this.activity = activity;
     this.coordinationLevel = coordinationLevel;
+    this.path = [{ x: x, y: y }];
   }
 
-  draw() {
+  // Function to draw a circle at the current position
+  drawCircle() {
     context.beginPath();
     context.strokeStyle = this.color;
     context.lineWidth = 3;
@@ -27,11 +35,48 @@ class Circle {
     context.stroke();
   }
 
+  // Function to draw the path taken so far
+  drawPath() {
+    const length = this.path.length;
+    const firstPath = this.path[0];
+    const lastPath = this.path[length - 1];
+
+    // Draw a start point
+    context.beginPath();
+    context.fillStyle = 'rgba(255,255,255,0.4)';
+    context.moveTo(firstPath.x, firstPath.y);
+    context.arc(firstPath.x, firstPath.y, 8, 0, 2 * Math.PI);
+    context.fill();
+
+    // Draw a path
+    context.beginPath();
+    context.strokeStyle = this.color;
+    context.lineWidth = 2;
+    context.lineJoin = 'round';
+    context.lineCap = 'round';
+    context.moveTo(firstPath.x, firstPath.y);
+
+    for (let i = 1; i < length; i += 60) {
+      context.lineTo(this.path[i].x, this.path[i].y);
+    }
+    context.lineTo(lastPath.x, lastPath.y);
+    context.stroke();
+
+    // Draw a end point
+    context.beginPath();
+    context.fillStyle = this.color;
+    context.moveTo(lastPath.x, lastPath.y);
+    context.arc(lastPath.x, lastPath.y, 4, 0, 2 * Math.PI);
+    context.fill();
+  }
+
+  // Function to update the next position without considering the other position
   normalUpdate() {
     this.x += random(-this.activity, this.activity);
     this.y += random(-this.activity, this.activity);
   }
 
+  // Function to update the next position to coordinate with the other
   cooperationUpdate() {
     const other = circles.find((circle) => circle.name !== this.name);
 
@@ -60,6 +105,7 @@ class Circle {
     }
   }
 
+  // Function to avoid collisions to the four corners
   check() {
     if (this.x + this.size >= width) {
       this.x += -this.activity;
@@ -76,6 +122,11 @@ class Circle {
     if (this.y - this.size <= 0) {
       this.y += this.activity;
     }
+  }
+
+  // Function to save path
+  save() {
+    this.path.push({ x: this.x, y: this.y });
   }
 }
 
@@ -115,12 +166,16 @@ let circles = [
   new Circle('circle2', width / 2, height / 2, 'rgb(82,158,175)', 40, 3, null), // Blue circle
 ]; */
 
-function animation() {
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function startDrawCircle() {
   context.fillStyle = 'rgba(0,0,0,0.25)';
   context.fillRect(0, 0, width, height);
 
   for (let i = 0; i < circles.length; i++) {
-    circles[i].draw();
+    circles[i].drawCircle();
 
     if (circles[i].coordinationLevel === null) {
       circles[i].normalUpdate();
@@ -138,9 +193,53 @@ function animation() {
     }
 
     circles[i].check();
+    circles[i].save();
   }
 
-  requestAnimationFrame(animation);
+  requestID = requestAnimationFrame(startDrawCircle);
 }
 
-animation();
+function startDrawPath() {
+  context.fillStyle = 'rgb(0,0,0)';
+  context.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].drawPath();
+  }
+
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].drawCircle();
+  }
+}
+
+function handleClick() {
+  if (isPlaying) {
+    clickStop();
+  } else {
+    clickPlay();
+  }
+  isPlaying = !isPlaying;
+}
+
+function clickPlay() {
+  viewStopIcon();
+  startDrawCircle();
+}
+
+function clickStop() {
+  viewPlayIcon();
+  cancelAnimationFrame(requestID);
+  startDrawPath();
+}
+
+function viewPlayIcon() {
+  playIcon.style.cssText = '';
+  stopIcon.style.cssText = 'display:none';
+}
+
+function viewStopIcon() {
+  playIcon.style.cssText = 'display:none';
+  stopIcon.style.cssText = '';
+}
+
+startDrawCircle();
